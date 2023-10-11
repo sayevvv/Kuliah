703037d802d74748fa646bef094c9191516e433b
sayevvv <shamilbasayev10@gmail.com>
2023-10-11 12:45:15 +0700

    Modifikasi mencari rata rata :fire:

--- Daspro Semester 1/Jobsheet 7/ForKelipatan01.java
+++ Daspro Semester 1/Jobsheet 7/ForKelipatan01.java
@@ -4,6 +4,7 @@ public class ForKelipatan01 {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0;
+        int rataRata;
         
 
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
@@ -11,12 +12,15 @@ public class ForKelipatan01 {
 
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
-                jumlah += 1;
+                jumlah += i;
                 counter++;
             }
         }
+        rataRata = jumlah / counter;
+        System.out.printf("Rata rata dari seluruh bagian kelipatan adalah %d\n",rataRata);
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        
     Sc.close();
     }
 }
