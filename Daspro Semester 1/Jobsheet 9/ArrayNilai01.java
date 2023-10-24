896d5c334ba6417ac3a083b7caff1e3967299d01
sayevvv <shamilbasayev10@gmail.com>
2023-10-24 12:44:13 +0700

    Push Percobaan 2 :bookmark:

---
+++ Daspro Semester 1/Jobsheet 9/ArrayNilai01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayNilai01 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int [] nilaiAkhir = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = Sc.nextInt();
+
+
+            }
+        }
+    }
+
