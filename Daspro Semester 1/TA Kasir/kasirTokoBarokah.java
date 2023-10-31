72b26f3bb427fa2240fcd1fb81d59bde94a9f865
sayevvv <shamilbasayev10@gmail.com>
2023-10-31 13:06:29 +0700

    Modifikasi TA dengan array :bookmark:

--- Daspro Semester 1/TA Kasir/kasirTokoBarokah.java
+++ Daspro Semester 1/TA Kasir/kasirTokoBarokah.java
@@ -1,112 +1,56 @@
 import java.util.Scanner;
 
 public class kasirTokoBarokah {
-      public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        String[] listBarang = {"Roti", "Beras", "Telur", "Minyak", "Gula", "Snack", "Rokok", "Mie " };
+        double[] harga = {20000, 50000, 15000, 25000, 12000, 8000, 25000, 4000};
 
-                Scanner input = new Scanner(System.in);
-                
-                // Deklarasi variabel
-                double totalHarga = 0.0, hargaBarang = 0;
-                boolean lanjutBelanja = true, barangValid = true;
-                int roti = 20000, beras = 50000, telur = 15000, minyak = 25000, gula = 12000, snack = 8000, rokok = 25000, mie = 4000;
-                String namaBarang;
-
-                System.out.println("Toko Barokah");
-                System.out.println("==========================");
-                System.out.println("List Barang Toko Barokah : ");
-                System.out.println("Roti \t\t: 20000");
-                System.out.println("Beras \t\t: 50000");
-                System.out.println("Telur 1 kg \t: 15000");
-                System.out.println("Minyak \t\t: 25000");
-                System.out.println("Gula \t\t: 12000");
-                System.out.println("Snack \t\t: 8000");
-                System.out.println("Mie \t\t: 4000");
-                System.out.println("Rokok \t\t: 25000");
-                System.out.println("==========================");
-
-                System.out.println("Kasir Toko Barokah");
-                
-                while (lanjutBelanja) {
-                    System.out.println("==============================================");
-                    do {
-                        System.out.print("Masukkan nama barang yang anda beli \t: ");
-                        namaBarang = input.next(); // INPUT NAMA BARANG
-                    
-                        switch(namaBarang.toLowerCase()) {
-                        case "roti" :
-                        hargaBarang = roti;
-                        break;
-                        case "beras" :
-                        hargaBarang = beras;
-                        break;
-                        case "telur" :
-                        hargaBarang = telur;
-                        break;
-                        case "minyak" :
-                        hargaBarang = minyak;
-                        break;
-                        case "gula" :
-                        hargaBarang = gula;
-                        break;
-                        case "snack" :
-                        hargaBarang = snack;
-                        break;
-                        case "rokok" :
-                        hargaBarang = rokok;
-                        break;
-                        case "mie" :
-                        hargaBarang = mie;
-                        break;
-                        default :
-                        System.out.println("Barang tidak tersedia atau anda salah input");
-                        barangValid = false;
-                        break;
-                         } 
-                    } while(barangValid = false);
-                    
-                    
-                    System.out.print("Masukkan jumlah barang \t: ");
-                    int jumlah = input.nextInt(); // INPUT JUMLAH BARANG
-                    
-                    double totalBarang = hargaBarang * jumlah; // OPERASI BARANG DIKALI JUMLAH
-                    totalHarga += totalBarang; // OPERASI BARANG DIMASUKKAN KE DALAM TOTAL HARGA
-                    
-                    System.out.println(namaBarang + " seharga " + hargaBarang  + " x "+ jumlah + " = " + totalBarang );
-                    
-                    System.out.print("Apakah kamu mau lanjut belanja? (ya/tidak): ");
-                    String lanjut = input.next();
-                    lanjutBelanja = lanjut.equalsIgnoreCase("ya");
-                }
-                
-                System.out.println("Total Harga: IDR " + totalHarga);
-                
-                System.out.print("Masukkan uang pembeli\t: IDR ");
-                double uangPembeli = input.nextDouble(); // INPUT UANG PEMBELI
-                
-                double kembalian = uangPembeli - totalHarga; // OPERASI KEMBALIAN
-                
-                if (kembalian >= 0) {
-                    System.out.println("Kembalian Anda \t: " + kembalian); // OUTPUT KEMBALIAN
-                } else {
-                    System.out.println("Uang tidak cukup"); // OUTPUT UANG TIDAK CUKUP
-                }
-                
-                input.close();
-                System.out.println("Terimakasih!");
-                System.out.println("==============================================");
+        int[] keranjang = new int[listBarang.length];
+        double totalHarga = 0;
+       
+            System.out.println("List Toko Barokah : ");
+            for (int i = 0; i < listBarang.length; i++) {
+                System.out.println((i+1) + ". " + listBarang[i] + " \tIDR " + harga[i]);
             }
+        System.out.println("==========================");
+        while (true) {
+            System.out.print("Masukkan nomor barang : ");
+            int pilihan = Sc.nextInt();
+
+        if (pilihan == 0) {
+            break;
+        }
+        if (pilihan < 1 || pilihan > listBarang.length) {
+            System.out.println("Input nomor barang tidak valid");
+            continue;
+        }
+        int indexBarang = pilihan - 1;
+        keranjang[indexBarang]++;
+
+
+        System.out.println("Kamu telah menambahkan " +listBarang[indexBarang] +  " ke dalam keranjang");
+        totalHarga += harga[indexBarang];
         }
         
-        
-
-    
-
-
-
-
-
-
-
-       
-    
-    
+        System.out.println("Keranjang anda : ");
+        for (int i = 0; i < listBarang.length; i++) {
+            if (keranjang[i] > 0) {
+                System.out.println(listBarang[i] + " sejumlah-" + keranjang[i] + " : IDR " + (keranjang[i]*harga[i]));
+            }
+        }
+        System.out.println("Harga Total : IDR " + totalHarga);
+        System.out.print("Masukkan uang pembeli\t: IDR ");
+                double uangPembeli = Sc.nextDouble(); 
+                
+                double kembalian = uangPembeli - totalHarga; 
+                
+                if (kembalian >= 0) {
+                    System.out.println("Kembalian \t: " + kembalian); 
+                } else {
+                    System.out.println("Uang tidak cukup"); 
+                }
+        Sc.close();
+                
+        }
+    }
