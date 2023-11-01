12321e2fcd318477d204cfe7bdf78d9c49d2c82b
sayevvv <shamilbasayev10@gmail.com>
2023-11-01 08:39:45 +0700

    Modifikasi Pertanyaan 1 Nomer 11 :zap:

--- Daspro Semester 1/Jobsheet 10/Bioskop01.java
+++ Daspro Semester 1/Jobsheet 10/Bioskop01.java
@@ -22,9 +22,9 @@ public class Bioskop01 {
             System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton.length);
         }
         System.out.println("Penonton pada baris ke-3: ");
-        
-        for(int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+
+        for(String i : penonton[2]) {
+            System.out.println(i);
         }
        
      }
