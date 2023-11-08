cebc6f2367a40f8b7e728883a6c4d3a07c69cc02
sayevvv <shamilbasayev10@gmail.com>
2023-11-08 15:18:19 +0700

    Modifikasi Praktikum 5 :zap:

---
+++ Daspro Semester 1/Jobsheet 11/NestedLoop_2341720166.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NestedLoop_2341720166 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = scanner.nextDouble();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
+            }
+            double total = 0;
+            for (double temp : temps[i]) {
+                total += temp;
+            }
+            double ratarata = total / temps[i].length;
+            System.out.println("Rata-rata: " + ratarata);
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
